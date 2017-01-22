ba5a03a4dbe85747dd72902655b58d335411a382
StevenGraver <S.Genseleiter@web.de>
2017-01-22 22:21:25 +0100

    Angepasste Map noch ohne Datenbank abfrage

--- EventPlaner_v1/WebContent/js/CustomGoogleMapMarker.js
+++ EventPlaner_v1/WebContent/js/CustomGoogleMapMarker.js
@@ -1,57 +1,54 @@
-function CustomMarker(latlng, map, args) {
-	this.latlng = latlng;	
-	this.args = args;	
-	this.setMap(map);	
-}
+$(document).ready(function () { /** Here hte function will start when HTML is finish with loading*/
+	
+	var map;			/** map is initialize*/
+    var myOptions = {								/** preproperties for the map*/
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map($('#map_canvas')[0], myOptions); /**define the map */
 
-CustomMarker.prototype = new google.maps.OverlayView();
+    var addresses = [' Hochschule Furtwangen, Robert-Gerwig-Platz, 78120 Furtwangen im Schwarzwald, Deutschland', /** Test the addresses*/
+                     ' BaumannstraÃŸe 7, 78120 Furtwangen im Schwarzwald, Deutschland', 
+                     ' LuisenstraÃŸe 20, 78120 Furtwangen im Schwarzwald, Deutschland',
+                     ' HinterschÃ¼tzenbach 6, 78120 Furtwangen im Schwarzwald, Deutschland',
+                     ' LochhofstraÃŸe 19, 78120 Furtwangen im Schwarzwald, Deutschland',
+                     ' Am GroÃŸhausberg 9, 78120 Furtwangen im Schwarzwald, Deutschland',
+                     ' Singen'];
+    var bounds = new google.maps.LatLngBounds();
+    for (var x = 0; x < addresses.length; x++) {
+    if(x==0){ /** The first address is the one for the event */
+    	$.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
+            var p = data.results[0].geometry.location;
+            var latlng = new google.maps.LatLng(p.lat, p.lng);
+            new google.maps.Marker({ /** set the eventmarker*/
+                position: latlng,
+                map: map,
+                icon : 'img/flag.png',
+                title: 'Event'
+            });
+            map.setCenter(p);	/** center the map to the event */
+            var loc = new google.maps.LatLng(p.lat,p.lng);
+            bounds.extends(loc);
+            
+        });
+    }else{
+    	$.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
+            var p = data.results[0].geometry.location;
+            var latlng = new google.maps.LatLng(p.lat, p.lng);
+            new google.maps.Marker({	/** set the markers for the eventmembers*/
+                position: latlng,
+                map: map,
+                icon : 'img/person.png',
+                title: 'Eventteilnehmer'
+            });
+            var loc = new google.maps.LatLng(p.lat,p.lng);
+            bounds.extends(loc);
 
-CustomMarker.prototype.draw = function() {
-	
-	var self = this;
-	
-	var div = this.div;
-	
-	if (!div) {
-	
-		div = this.div = document.createElement('div');
-		
-		div.className = 'marker';
-		
-		div.style.position = 'absolute';
-		div.style.cursor = 'pointer';
-		div.style.width = '20px';
-		div.style.height = '20px';
-		div.style.background = 'blue';
-		
-		if (typeof(self.args.marker_id) !== 'undefined') {
-			div.dataset.marker_id = self.args.marker_id;
-		}
-		
-		google.maps.event.addDomListener(div, "click", function(event) {
-			alert('You clicked on a custom marker!');			
-			google.maps.event.trigger(self, "click");
-		});
-		
-		var panes = this.getPanes();
-		panes.overlayImage.appendChild(div);
-	}
-	
-	var point = this.getProjection().fromLatLngToDivPixel(this.latlng);
-	
-	if (point) {
-		div.style.left = (point.x - 10) + 'px';
-		div.style.top = (point.y - 20) + 'px';
-	}
-};
-
-CustomMarker.prototype.remove = function() {
-	if (this.div) {
-		this.div.parentNode.removeChild(this.div);
-		this.div = null;
-	}	
-};
-
-CustomMarker.prototype.getPosition = function() {
-	return this.latlng;	
-};
+            
+        });
+    }
+    
+    
+    }
+    map.fitBounds(bounds);
+});
